9e564f19f424f30b14d3f924a1421b51ad0635ce
ehddbs4521 <ehddbs452100@naver.com>
2024-01-31 22:23:04 +0900

    first commit

---
+++ src/test/java/backend/refrigeratorcleaner/RefrigeratorCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.refrigeratorcleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigeratorCleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
